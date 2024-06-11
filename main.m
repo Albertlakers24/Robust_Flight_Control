7eb625eadecb2460a91117349e8c706a335d3971
Albert Chou <albertchou2000@hotmail.com>
2024-06-11 18:07:01 +0200

    Add main file

---
+++ main.m
@@ -0,0 +1,15 @@
+%%Part #1 System Modeling
+
+%% Part #2 Loop Shaping
+
+%% Part #3a Weighting filters
+
+%% Part #3b Reference model
+
+%% Part #3c Feedback controller design (hinfsyn case)
+
+%% Part #3d Feedback controller desgin (hinfstruct case)
+
+%% Part #3eFeedforward controller design
+
+%% Part #4 Feedback controller redesign (systune case) (Optional)
