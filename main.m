0bab297850f853be5e29ba9be022553e276f1ded
Albert Chou <albertchou2000@hotmail.com>
2024-06-23 19:30:48 +0200

    Completed Q2.1 and 2.2

--- main.m
+++ main.m
@@ -30,10 +30,10 @@ G_m.StateName = {'x1', 'x2'};
 G_m.OutputName = {'y1', 'y2'};
 
 % Actuator model
-A_a = [0 1; -omega_a^2 -2*zeta_a*omega_a]; % A matrix missile
-B_a = [0; omega_a^2;]; % B Matrix missile
-C_a = [1 0; 0 1]; % C Matrix missile
-D_a = [0; 0]; % D matrix missile
+A_a = [0 1; -omega_a^2 -2*zeta_a*omega_a]; % A matrix actuator
+B_a = [0; omega_a^2;]; % B Matrix actuator
+C_a = [1 0; 0 1]; % C Matrix actuator
+D_a = [0; 0]; % D matrix actuator
 
 G_a = ss(A_a, B_a, C_a, D_a);
 G_a.InputName = 'u_cmd';
@@ -67,6 +67,30 @@ grid on;
 title('Pole-Zero Map');
 
 %% Part #2 Loop Shaping
+
+% Damping gain tuning
+clc;
+figure;
+rlocusplot(-G_am(2,1));
+grid on;
+title('Root Locus Plot');
+C_q_gain = -0.163; % C_q gain obtained from root locus plot for CL damping of 0.7
+linsys_cq = linearize('ClosedLoop_Cq');
+G_cl_q = ss2ss(linsys_cq,T);
+G_cl_q_unsc = G_cl_q('y1','u_unsc');
+zpk_G_cl_q_unsc = zpk(G_cl_q_unsc);
+
+% Scaling gain Design
+C_sc_gain = 1/ dcgain(G_cl_q_unsc);
+linsys_csc = linearize("ClosedLoop_CqCsc");
+G = ss2ss(linsys_csc,T);
+G = G('y1','u_p');
+zpk_G = zpk(G);
+G_tf = tf(G);
+figure;
+step(G_tf);
+grid on;
+title("Step Reponse of G");
 
 %% Part #3a Weighting filters
 
